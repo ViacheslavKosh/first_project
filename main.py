45edddab802b1860405f5017682d5cae86659b95
ViacheslavKosh <koshelevskiy.v@gmail.com>
2024-02-17 22:38:20 +0200

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 print ("WTF")
+# Введення (отримання даних)
+ім_я = input("Введіть ваше ім'я: ")
+
+# Перетворення (обробка даних)
+вітання = f"Привіт, {ім_я}!"
+
+# Виведення (виведення даних)
+print(вітання)
+print(ім_я[2])
+int_number = 3
+float_number = 3.3
+complex_number = 3 + 3.5j
+
+is_active = True
+is_delete = False
+
+age = 18
+is_adult = age >= 18  # True
+print (is_adult)
+
+age = 15
+is_adult = age >= 18  # False
+print(is_adult)
