208edf8c7a34446a3de9c0fb1e2080d5ee95501f
ViacheslavKosh <koshelevskiy.v@gmail.com>
2024-03-07 22:06:25 +0200

    07032024

---
+++ Module_3/21_str_format.py
@@ -0,0 +1,25 @@
+for i in range(8):
+    s = f"int: {i:d};  hex: {i:#x};  oct: {i:#o};  bin: {i:#b}"
+    print(s)
+
+price = 19.99
+quantity = 3
+total = f"Total: {price * quantity:.2f}"
+print(total)
+
+width = 5
+for num in range(12):
+    print(f'{num:^10} {num**2:^10} {num**3:^10}')
+
+
+name = "Alice"
+formatted = f"{name:>10}"
+print(formatted)  # Виведе: '     Alice' (вирівнювання праворуч)
+
+
+completion = 0.756
+formatted = f"{completion:.1%}"
+print(formatted)  # Виведе: '75.6%'
+progress = 0.5
+formatted = f"{progress:.0%}"
+print(formatted)
