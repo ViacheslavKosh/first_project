53f24d31238343af770963187dd73bb1358e3328
viacheslavkosh <koshelevskiy.v@gmail.com>
2024-05-14 23:45:20 +0300

    now

--- DS_ML/module_3/main.py
+++ DS_ML/module_3/main.py
@@ -1,15 +1,36 @@
-
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://koshelevskiyv:<>@cluster0.ne3bbmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(
+    "mongodb+srv://<username>:<password>@krabaton.5mlpr.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    server_api=ServerApi('1')
+)
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+db = client.book
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+result_one = db.cats.insert_one(
+    {
+        "name": "barsik",
+        "age": 3,
+        "features": ["ходить в капці", "дає себе гладити", "рудий"],
+    }
+)
+
+print(result_one.inserted_id)
+
+result_many = db.cats.insert_many(
+    [
+        {
+            "name": "Lama",
+            "age": 2,
+            "features": ["ходить в лоток", "не дає себе гладити", "сірий"],
+        },
+        {
+            "name": "Liza",
+            "age": 4,
+            "features": ["ходить в лоток", "дає себе гладити", "білий"],
+        },
+    ]
+)
+print(result_many.inserted_ids)
+
